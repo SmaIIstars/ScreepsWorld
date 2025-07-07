aef99b5098f4936c78f5eade11222b22bb020f36
Tanner <smallstars.he@qq.com>
2025-07-07 09:27:31 +0800

    feat: 1.add min group

--- src/utils/monitor/task.ts
+++ src/utils/monitor/task.ts
@@ -4,8 +4,32 @@ import { BASE_ID_ENUM } from "@/constant";
 import { generatorRoleBody } from "../lib/base/role";
 
 const task = () => {
+  minCreepGroup();
   miner();
   minerStore();
+};
+
+const minCreepGroup = () => {
+  const creepsList: Array<{ name: string; role: CustomRoleType }> = [
+    { name: "Harvester-1", role: "harvester" },
+    { name: "Upgrader-1", role: "upgrader" },
+    { name: "Builder-1", role: "builder" },
+    { name: "Repairer-1", role: "repairer" },
+  ];
+
+  for (const creep of creepsList) {
+    const creepCreep = Game.creeps[creep.name];
+    if (!creepCreep) {
+      const spawnResult = utils.role[creep.role].create(BASE_ID_ENUM.MainBase, {
+        body: generatorRoleBody([
+          { body: WORK, count: 1 },
+          { body: CARRY, count: 2 },
+          { body: MOVE, count: 2 },
+        ]),
+        opts: { memory: { role: creep.role, task: "harvesting" } },
+      });
+    }
+  }
 };
 
 const miner = () => {
