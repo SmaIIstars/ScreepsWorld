77a680b37b385e5f120c56ac91cbec0e6887b306
Tanner <smallstars.he@qq.com>
2026-07-02 03:29:55 +0800

    feat: add BaseStructure abstract class

---
+++ src/structures/BaseStructure.ts
@@ -0,0 +1,23 @@
+// src/structures/BaseStructure.ts
+import { Guild, DemandData } from '../core/Guild';
+import { buildDedupKey } from '../core/Event';
+
+export abstract class BaseStructure<T extends Structure> {
+  protected obj: T;
+  protected room: Room;
+
+  constructor(obj: T) {
+    this.obj = obj;
+    this.room = obj.room;
+  }
+
+  abstract runLifecycle(): void;
+
+  protected post(demand: DemandData): void {
+    Guild.post(demand);
+  }
+
+  protected cancel(type: string): void {
+    Guild.cancel(buildDedupKey(type, this.room.name, this.obj.id));
+  }
+}
