eb5ee9e1d63b6a614c5725f9fb4fe004e9a573ca
Tanner <smallstars.he@qq.com>
2026-07-02 17:14:44 +0800

    feat: add RCL 3 strategy config (bigger creeps, tower support)

--- src/strategy/index.ts
+++ src/strategy/index.ts
@@ -23,21 +23,47 @@
     roles: [
       {
         type: 'harvester',
-        body: [WORK, WORK, CARRY, MOVE, CARRY, MOVE],
+        body: [WORK, WORK, CARRY, MOVE, CARRY, CARRY, MOVE],
         tags: ['harvest', 'transport', 'move'],
         minCapacities: { harvest: 1, carry: 50 },
         target: 2,
       },
       {
         type: 'upgrader',
-        body: [WORK, WORK, CARRY, MOVE, CARRY, MOVE],
+        body: [WORK, WORK, CARRY, MOVE, CARRY, CARRY, MOVE],
         tags: ['work', 'transport', 'move'],
         minCapacities: { work: 1, carry: 50 },
         target: 2,
       },
       {
         type: 'builder',
-        body: [WORK, WORK, CARRY, MOVE, CARRY, MOVE],
+        body: [WORK, WORK, CARRY, MOVE, CARRY, CARRY, MOVE],
+        tags: ['work', 'transport', 'move'],
+        minCapacities: { work: 1, carry: 50 },
+        target: 3,
+      },
+    ],
+  },
+  3: {
+    level: 3,
+    roles: [
+      {
+        type: 'harvester',
+        body: [WORK, WORK, WORK, CARRY, CARRY, MOVE, MOVE],
+        tags: ['harvest', 'transport', 'move'],
+        minCapacities: { harvest: 1, carry: 50 },
+        target: 2,
+      },
+      {
+        type: 'upgrader',
+        body: [WORK, WORK, CARRY, CARRY, MOVE, MOVE],
+        tags: ['work', 'transport', 'move'],
+        minCapacities: { work: 1, carry: 50 },
+        target: 2,
+      },
+      {
+        type: 'builder',
+        body: [WORK, WORK, CARRY, CARRY, MOVE, MOVE],
         tags: ['work', 'transport', 'move'],
         minCapacities: { work: 1, carry: 50 },
         target: 3,
